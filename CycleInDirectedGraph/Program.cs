ddcfc6bd7507c37f5981056b28487fb78c73dcdf
smit1304 <95951014+smit1304@users.noreply.github.com>
2022-09-12 23:56:33 +0530

    DFS

--- CycleInDirectedGraph/Program.cs
+++ CycleInDirectedGraph/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Graph
+{
+    public int numOfVertices;
+    public int[,] adjacencyMatrix;
+
+    public Graph(int v)
+    {
+        this.numOfVertices = v;
+        adjacencyMatrix = new int[v, v];
+    }
+
+    public void _AddEdge(int src, int dest)
+    {
+
+    }
+}
+class CycleInDirectedGraph
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
