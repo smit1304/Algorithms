480b33f8a379ff7d4b39e14f5528cf65bf221098
smit1304 <95951014+smit1304@users.noreply.github.com>
2022-09-13 00:25:16 +0530

    DFS

--- CycleInDirectedGraph/Program.cs
+++ CycleInDirectedGraph/Program.cs
@@ -1,4 +1,16 @@
-﻿class Graph
+﻿class Node
+{
+    int value;
+    bool visited;
+
+    public Node(int val)
+    {
+        this.value = val;
+        this.visited = false;
+    }
+}
+
+class Graph
 {
     public int numOfVertices;
     public int[,] adjacencyMatrix;
@@ -11,13 +23,32 @@
 
     public void _AddEdge(int src, int dest)
     {
+        this.adjacencyMatrix[src, dest] = 1;
+    }
 
+    public void _PrintGraph()
+    {
+        for (int i = 0; i < numOfVertices; i++)
+        {
+            for (int j = 0; j < numOfVertices; j++)
+            {
+                if (this.adjacencyMatrix[i, j] == 1)
+                {
+                    Console.WriteLine("edge between " + i + " and " + j);
+                }
+            }
+        }
     }
 }
 class CycleInDirectedGraph
 {
     public static void Main(string[] args)
     {
+        Graph g = new Graph(3);
+        g._AddEdge(0, 1);
+        g._AddEdge(1, 2);
+        g._AddEdge(2, 0);
+        g._PrintGraph();
 
     }
 }
