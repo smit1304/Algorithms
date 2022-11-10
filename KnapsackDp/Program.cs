b2f0e67eaea30e00ec96871d7c8a4d27bc0465c5
smit1304 <95951014+smit1304@users.noreply.github.com>
2022-11-10 17:46:27 +0530

    10-11-22 (update)

---
+++ KnapsackDp/Program.cs
@@ -0,0 +1,75 @@
+﻿class Item
+{
+    public int weight;
+    public int value;
+    //public float pwRatio;
+    public Item(int v1, int v2)
+    {
+        this.value = v1;
+        this.weight = v2;
+    }
+}
+
+class KnapsackDp
+{
+    public static void _TraceKnapackDp()
+    {
+
+    }
+    public static void _FillTable(float[,] records, List<Item> listOfObjects, int outerBound, int innerBound)
+    {
+        for (int i = 0; i < outerBound; i++)
+        {
+            for (int j = 0; j < innerBound; j++)
+            {
+                if (j == 0)
+                {
+                    records[i, j] = 0;
+                    return;
+                }
+                if (j < listOfObjects[i - 1].weight)
+                {
+                    records[i, j] = records[i - 1, j];
+                    return;
+                }
+                if (j >= listOfObjects[i - 1].weight)
+                {
+                    records[i, j] = MathF.Max(records[i - 1, j], records[i - 1, j - listOfObjects[i - 1].weight] + listOfObjects[i - 1].value);
+                    return;
+                }
+
+            }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Size of bag : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        List<Item> listOfObjects = new List<Item>()
+        {
+            new Item(22,4),new Item(12,8),new Item(8,2),new Item(21,7),new Item(15,5) //(profit,value)
+        };
+        //int[] Di = new int[] { 1, 4, 6 };
+        float[,] records = new float[listOfObjects.Count + 1, n + 1];
+        List<float> solution = new List<float>();
+
+        _FillTable(records, listOfObjects, listOfObjects.Count + 1, n + 1);
+        //_TraceKnapackDp();
+
+        // Debug
+        Console.WriteLine("Taable : ");
+        for (int i = 0; i < listOfObjects.Count + 1; i++)
+        {
+            for (int j = 0; j < n + 1; j++)
+            {
+                Console.Write(records[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        // Console.Write("Solution : ");
+        // for (int i = 0; i < solution.Count; i++)
+        // {
+        //     Console.Write(solution[i] + " ");
+        // }
+    }
+}
---
+++ PizzaOrderingSystem/Program.cs
@@ -0,0 +1,85 @@
+﻿class Pizza
+{
+    public string Size;
+    public List<String> ToppingAdded = new List<String>();
+}
+
+class Order
+{
+    public string Size;
+    public List<String> ToppingAdded = new List<String>();
+    public static List<String> Topings = new List<string>(){
+            "ONION","TOMATO","GREEN PAPER","MASHROOM","OLIVE","SPINACH","BROCCOLY",
+            "PINEAPPLE","HOT PAPER","PEPRONI","HAM","BECON","BROWN BEAF","CHIKEN",
+            "SAUSAGE"
+    };
+    public static void Main(string[] args)
+    {
+        Order order = new Order();
+        Console.Write("Do you want to order pizza ? ");
+        string answer = Console.ReadLine();
+
+
+        if (answer == "NO" || answer == "Q")
+        {
+            return;
+        }
+
+        //select size of pizza
+        do
+        {
+            Console.Write("size : ");
+            order.Size = Console.ReadLine();
+
+        } while (order.Size != "S" &&
+                 order.Size != "M" &&
+                 order.Size != "L" &&
+                 order.Size != "XL");
+
+
+        // Topping Zone
+
+        do
+        {
+        here:
+            Console.Write("Type in one our topping to add into your pizza to see the list of topping enter 'LIST' when you are done adding topping enter 'X' : ");
+            string response = Console.ReadLine();
+
+            if (response == "LIST")
+            {
+                Console.WriteLine("These are the Topings : ");
+                foreach (string s in Topings)
+                {
+                    Console.Write(s + " ");
+                }
+                continue;
+            }
+            foreach (string s in Topings)
+            {
+                if (s.Equals(response))
+                {
+                    Console.WriteLine(s + " Topping Added to pizza");
+                    goto here;
+                }
+            }
+            if (response == "X")
+            {
+                break;
+            }
+
+            Console.WriteLine("Input invalid !");
+
+            goto here;
+
+        } while (true);
+        //Console.WriteLine(order.Size);
+
+
+
+    }
+}
+
+class PizzaResipt
+{
+
+}
